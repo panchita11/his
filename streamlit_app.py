d82e49c41aea09908d74fc68c66acac652b19907
panchita11 <teran110805@gmail.com>
2024-11-20 16:16:02 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
